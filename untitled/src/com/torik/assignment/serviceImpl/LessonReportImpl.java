e819d2bc0dd1efad33d515042838563bdce3d17a
Golam Yusuf <yusuf_siffo@yahoo.com>
2023-04-12 13:02:12 +0600

    step 13 work is done

--- untitled/src/com/torik/assignment/serviceImpl/LessonReportImpl.java
+++ untitled/src/com/torik/assignment/serviceImpl/LessonReportImpl.java
@@ -5,8 +5,8 @@ import com.torik.assignment.bean.Lesson;
 import com.torik.assignment.bean.Rating;
 import com.torik.assignment.service.LessonReports;
 
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class LessonReportImpl implements LessonReports {
     @Override
@@ -17,22 +17,28 @@ public class LessonReportImpl implements LessonReports {
                     rate -> System.out.println(rate.getLessonName()));
 
             double avgValue = listRating.stream().filter(
-                    rate -> rate.getLessonName().equalsIgnoreCase(
-                            lesson.getLessonName())
-            ).mapToDouble(
-                    rating -> rating.getRating())
+                            rate -> rate.getLessonName().equalsIgnoreCase(
+                                    lesson.getLessonName())
+                    ).mapToDouble(
+                            rating -> rating.getRating())
                     .average().orElse(0.0);
-            System.out.println("Lesson Name: " + lesson.getLessonName() + "  " +avgValue);
+            System.out.println("Lesson Name: " + lesson.getLessonName() + "  " + avgValue);
         });
     }
 
     @Override
     public void generateLessonReportBasedOnIncome(List<Lesson> listLesson, List<BookingOrder> listBookingOrder) {
         HashMap<String, Double> hmap = new HashMap<String, Double>();
-        listBookingOrder.stream().forEach(x-> x.getListLesson().stream().forEach( y-> {
-            hmap.computeIfPresent(y.getLessonName() ,(key,value) -> value + (y.getPrice()*y.getNumOfCustomer()));
-        }));
+        listLesson.stream().forEach(y -> {
+            hmap.put(y.getLessonName(), (y.getNumOfCustomer() * y.getPrice()));
 
-        hmap.forEach((x,y) -> System.out.println("Lesson "+x +"  Profit "+y));
+        });
+
+        hmap.entrySet()
+                .stream()
+                .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder())) ;
+
+
+        hmap.forEach((x, y) -> System.out.println("Lesson " + x + "  Profit " + y));
     }
 }
